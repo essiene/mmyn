558cdbc257a5b9fedec8cf11b6f91aeda731d37b
Essien Ita Essien <essiene@gmail.com>
2010-05-06 14:27:15 +0100

    Initial generated templates by rebar
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/simreg_app.erl
@@ -0,0 +1,16 @@
+-module(simreg_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    simreg_sup:start_link().
+
+stop(_State) ->
+    ok.
