3245478922a6be8ebad660b4e5d713530eb2a3c7
Essien Ita Essien <essiene@gmail.com>
2010-05-07 15:00:09 +0100

    Implement Named RX processes.
    Each rx process now has an ID which it holds in its
     #state{} and uses for logging.
    
    Use this opportunity to clean up the error logging in
    this module.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/simreg_rx.erl
+++ src/simreg_rx.erl
@@ -12,43 +12,44 @@
         terminate/2,
         code_change/3]).
 
--export([start_link/0, start/0, stop/1]).
+-export([start_link/1, start/1, stop/1]).
 
--record(state, {host, port, system_id, password, smpp, callback}).
+-record(state, {host, port, system_id, password, smpp, callback, id}).
 
-start_link() ->
-    gen_esme:start_link(?MODULE, [?SMSC_HOST, ?SMSC_PORT, ?SYSTEM_ID, ?PASSWORD, simreg_services], []).
+start_link(Id) ->
+    gen_esme:start_link(?MODULE, [?SMSC_HOST, ?SMSC_PORT, ?SYSTEM_ID, ?PASSWORD, simreg_services, Id], []).
 
-start() ->
-    gen_esme:start(?MODULE, [?SMSC_HOST, ?SMSC_PORT, ?SYSTEM_ID, ?PASSWORD, simreg_services], []).
+start(Id) ->
+    gen_esme:start(?MODULE, [?SMSC_HOST, ?SMSC_PORT, ?SYSTEM_ID, ?PASSWORD, simreg_services, Id], []).
 
 stop(Pid) ->
     gen_esme:cast(Pid, stop).
 
-init([Host, Port, SystemId, Password, Callback]) ->
+init([Host, Port, SystemId, Password, Callback, Id]) ->
     {ok, {Host, Port, 
             #bind_receiver{system_id=SystemId, password=Password}}, 
-            #state{host=Host, port=Port, system_id=SystemId, password=Password, callback=Callback}}.
+            #state{host=Host, port=Port, system_id=SystemId, password=Password,
+                callback=Callback, id=Id}}.
 
-handle_bind(Smpp, St) ->
+handle_bind(Smpp, #state{id=Id}=St) ->
+    error_logger:info_msg("Receiver ~p bound. Smpp: ~p~n", [Smpp]),
     {noreply, St#state{smpp=Smpp}}.
 
-handle_pdu(#pdu{body=#deliver_sm{source_addr=Src, destination_addr=Dst, short_message=Msg}}=Pdu, #state{callback=Callback}=St) ->
-    error_logger:info_report([simreg_rx, {instance, self()}, {deliver_sm, Pdu}]),
+handle_pdu(#pdu{body=#deliver_sm{source_addr=Src, destination_addr=Dst, short_message=Msg}}=Pdu, #state{callback=Callback, id=Id}=St) ->
+    error_logger:info_msg("Receiver ~p received PDU: ~p~n", [Pdu]),
     {ok, WordList} = preprocess(Msg),
     Callback:handle_sms(Src, Dst, WordList),
     {noreply, St};
 
-handle_pdu(Pdu, St) ->
-    error_logger:info_report([simreg_rx, {instance, self()}, {unhandled_pdu, Pdu}]),
+handle_pdu(Pdu, #state{id=Id}=St) ->
+    error_logger:info_msg("Receiver ~p received PDU: ~p~n", [Pdu]),
     {noreply, St}.
     
 handle_unbind(_Pdu, St) ->
     {noreply, St}.
 
 handle_call({sendsms, Source, Dest, Msg}, _From, #state{smpp=Smpp}=St) ->
-    S = smpp:send(Smpp, #submit_sm{source_addr=Source, destination_addr=Dest,
-            short_message=Msg}),
+    S = smpp:send(Smpp, #submit_sm{source_addr=Source, destination_addr=Dest, short_message=Msg}),
     {reply, S, St};
 handle_call(Req, _From, St) ->
     {reply, {error, Req}, St}.
@@ -58,11 +59,12 @@ handle_cast(stop, St) ->
 handle_cast(_Req, St) ->
     {noreply, St}.
 
-handle_info(Req, St) ->
-    error_logger:info_report([?MODULE, {unhandled_request, Req}]),
+handle_info(Req, #state{id=Id}=St) ->
+    error_logger:info_msg("Receiver ~p recieved non gen_server request: ~p", [Req]),
     {noreply, St}.
 
-terminate(_Reason, _St) ->
+terminate(Reason, #state{id=Id}) ->
+    error_logger:info_msg("Receiver ~p is terminating with reason: ~p~n", [Reason]),
     ok.
 
 code_change(_OldVsn, St, _Extra) ->
