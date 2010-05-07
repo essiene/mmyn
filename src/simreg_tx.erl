d68391f45a828c672579d177892b37b773cb5c35
Essien Ita Essien <essiene@gmail.com>
2010-05-07 22:08:03 +0100

    Implement periodic dequeing in simreg_tx
    With naive support for incremental backoff
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/simreg_tx.erl
+++ src/simreg_tx.erl
@@ -12,9 +12,14 @@
         terminate/2,
         code_change/3]).
 
--export([start_link/0, start/0, stop/0, sendsms/3]).
+-export([start_link/0, start/0, stop/0]).
 
--record(state, {host, port, system_id, password, smpp}).
+-define(WAIT_MAX, 360000).
+-define(WAIT_MIN, 100).
+-define(WAIT_GROW, 1000).
+-define(TXQ_CHK, txq_chk).
+
+-record(state, {host, port, system_id, password, smpp, wait}).
 
 start_link() ->
     gen_esme:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -25,29 +30,23 @@ start() ->
 stop() ->
     gen_esme:cast(?MODULE, stop).
 
-sendsms(Source, Dest, Msg) ->
-    gen_esme:call(?MODULE, {sendsms, Source, Dest, Msg}).
-
 init([]) ->
     {Host, Port, SystemId, Password} = util:smsc_params(),
     {ok, {Host, Port, 
             #bind_transmitter{system_id=SystemId, password=Password}}, 
-            #state{host=Host, port=Port, system_id=SystemId, password=Password}}.
+            #state{host=Host, port=Port, system_id=SystemId, password=Password, wait=?WAIT_MIN}}.
 
-handle_bind(Smpp, St) ->
+handle_bind(Smpp, #state{wait=Wait}=St) ->
+    wait(Wait),
     {noreply, St#state{smpp=Smpp}}.
 
 handle_pdu(Pdu, St) ->
-    error_logger:info_report([simreg_tx, {unhandled_pdu, Pdu}]),
+    error_logger:info_msg("simreg_tx has received an unhandled pdu: ~p~n", [Pdu]),
     {noreply, St}.
     
 handle_unbind(_Pdu, St) ->
     {noreply, St}.
 
-handle_call({sendsms, Source, Dest, Msg}, _From, #state{smpp=Smpp}=St) ->
-    S = smpp:send(Smpp, #submit_sm{source_addr=Source, destination_addr=Dest,
-            short_message=Msg}),
-    {reply, S, St};
 handle_call(Req, _From, St) ->
     {reply, {error, Req}, St}.
 
@@ -55,6 +54,15 @@ handle_cast(stop, St) ->
     {stop, normal, St};
 handle_cast(_Req, St) ->
     {noreply, St}.
+
+handle_info(?TXQ_CHK, #state{smpp=Smpp}=St) ->
+    case txq:pop() of 
+        '$empty' ->
+            wait_grow(St);
+        #txq_req{src=Src, dst=Dest, message=Msg} ->
+            smpp:send(Smpp, #submit_sm{source_addr=Src, destination_addr=Dest, short_message=Msg}),
+            wait_normal(St)
+    end;
 
 handle_info(_Req, St) ->
     {noreply, St}.
@@ -64,3 +72,23 @@ terminate(_Reason, _St) ->
 
 code_change(_OldVsn, St, _Extra) ->
     {noreply, St}.
+
+wait(N) when is_integer(N) ->
+    error_logger:info_msg("Going to wake up in ~p ms~n", [N]),
+    timer:send_after(N, ?TXQ_CHK);
+wait(#state{wait=Wait}) ->
+    wait(Wait).
+
+wait_grow(#state{wait=Wait}=St) ->
+    case Wait + ?WAIT_GROW of
+        N when N < ?WAIT_MAX ->
+            wait(N),
+            {noreply, St#state{wait=N}};
+        N ->
+            wait(N),
+            {noreply, St#state{wait=?WAIT_MAX}}
+    end.
+
+wait_normal(St) ->
+    wait(?WAIT_MIN),
+    {noreply, St#state{wait=?WAIT_MIN}}.
