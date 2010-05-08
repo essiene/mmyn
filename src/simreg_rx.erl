7c37f4978fdf7dc7d2fe940850ac80d5ee9ca868
Essien Ita Essien <essiene@gmail.com>
2010-05-08 10:51:19 +0100

    Rename #state{} to #st{}
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/simreg_rx.erl
+++ src/simreg_rx.erl
@@ -14,7 +14,7 @@
 
 -export([start_link/1, start/1, stop/1]).
 
--record(state, {host, port, system_id, password, smpp, callback, id}).
+-record(st, {host, port, system_id, password, smpp, callback, id}).
 
 start_link(Id) ->
     gen_esme:start_link(?MODULE, [simreg_services, Id], []).
@@ -29,27 +29,27 @@ init([Callback, Id]) ->
     {Host, Port, SystemId, Password} = util:smsc_params(),
     {ok, {Host, Port, 
             #bind_receiver{system_id=SystemId, password=Password}}, 
-            #state{host=Host, port=Port, system_id=SystemId, password=Password,
+            #st{host=Host, port=Port, system_id=SystemId, password=Password,
                 callback=Callback, id=Id}}.
 
-handle_bind(Smpp, #state{id=Id}=St) ->
+handle_bind(Smpp, #st{id=Id}=St) ->
     error_logger:info_msg("Receiver ~p bound. Smpp: ~p~n", [Id, Smpp]),
-    {noreply, St#state{smpp=Smpp}}.
+    {noreply, St#st{smpp=Smpp}}.
 
-handle_pdu(#pdu{body=#deliver_sm{source_addr=Src, destination_addr=Dst, short_message=Msg}}=Pdu, #state{callback=Callback, id=Id}=St) ->
+handle_pdu(#pdu{body=#deliver_sm{source_addr=Src, destination_addr=Dst, short_message=Msg}}=Pdu, #st{callback=Callback, id=Id}=St) ->
     error_logger:info_msg("Receiver ~p received PDU: ~p~n", [Id, Pdu]),
     {ok, WordList} = preprocess(Msg),
     Callback:handle_sms(Src, Dst, WordList),
     {noreply, St};
 
-handle_pdu(Pdu, #state{id=Id}=St) ->
+handle_pdu(Pdu, #st{id=Id}=St) ->
     error_logger:info_msg("Receiver ~p received PDU: ~p~n", [Id, Pdu]),
     {noreply, St}.
     
 handle_unbind(_Pdu, St) ->
     {noreply, St}.
 
-handle_call({sendsms, Source, Dest, Msg}, _From, #state{smpp=Smpp}=St) ->
+handle_call({sendsms, Source, Dest, Msg}, _From, #st{smpp=Smpp}=St) ->
     S = smpp:send(Smpp, #submit_sm{source_addr=Source, destination_addr=Dest, short_message=Msg}),
     {reply, S, St};
 handle_call(Req, _From, St) ->
@@ -60,11 +60,11 @@ handle_cast(stop, St) ->
 handle_cast(_Req, St) ->
     {noreply, St}.
 
-handle_info(Req, #state{id=Id}=St) ->
+handle_info(Req, #st{id=Id}=St) ->
     error_logger:info_msg("Receiver ~p recieved non gen_server request: ~p", [Id, Req]),
     {noreply, St}.
 
-terminate(Reason, #state{id=Id}) ->
+terminate(Reason, #st{id=Id}) ->
     error_logger:info_msg("Receiver ~p is terminating with reason: ~p~n", [Id, Reason]),
     ok.
 
