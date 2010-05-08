bceb5182f818e85276683f4f189b2d7ba300ed74
Essien Ita Essien <essiene@gmail.com>
2010-05-08 18:15:06 +0100

    Implement register/deregister in backoff.erl
    Also bring over the backoff logic in esmetx for handling
    the actual notification of clients.
    
    Instead of just delivering a message, we use timer:apply_after
    and hence we store an MFA that we can call back.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/backoff.erl
+++ src/backoff.erl
@@ -5,7 +5,10 @@
 
 -export([start_link/0, status/0, stop/0]).
 
+-export([register/4, deregister/0]).
+
 -record(st, {ets}).
+-record(spec, {pid, max, min, dlta, mfa, tref, cur, cur_dlta}).
 
 
 start_link() ->
@@ -17,10 +20,30 @@ status() ->
 stop() ->
     gen_server:call(?MODULE, stop).
 
+register(Min, Max, Delta, Mfa) ->
+    Pid = self(),
+    gen_server:call(?MODULE, {register, #spec{pid=Pid, max=Max, min=Min, dlta=Delta, mfa=Mfa}}).
+
+deregister() ->
+    Pid = self(),
+    gen_server:call(?MODULE, {deregister, Pid}).
 
 init([]) ->
     error_logger:info_msg("~p starting~n", [?MODULE]),
     {ok, #st{ets=ets:new(?MODULE, [set, private])}}.
+
+handle_call({register, #spec{pid=Pid}=Spec0}, _F, #st{ets=Ets}=St) ->
+    case backoff(Spec0) of
+        {ok, Spec} -> 
+            ets:insert(Ets, {Pid, Spec}), 
+            {reply, ok, St};
+        {error, Reason} ->
+            {reply, {error, Reason}, St}
+    end;
+
+handle_call({deregister, Pid}, _F, #st{ets=Ets}=St) ->
+    ets:delete(Ets, Pid),
+    {reply, ok, St};
 
 handle_call(status, _F, St) ->
     {reply, {ok, alive}, St};
@@ -47,3 +70,29 @@ terminate(_R, _St) ->
 
 code_change(_OldVsn, St, _Extra) ->
     {ok, St}.
+
+
+backoff(#spec{cur=N, tref=undefined, mfa={M, F, A}}=S) ->
+    case timer:apply_after(N, M, F, A) of
+        {ok, TRef} -> 
+            {ok, S#spec{tref=TRef}};
+        {error, Reason} ->
+            {error, Reason}
+    end;
+
+backoff(#spec{tref=TRef}=S) ->
+    timer:cancel(TRef),
+    backoff(S#spec{tref=undefined}).
+
+
+backoff_grow(#spec{cur=C, cur_dlta=D0, max=Max}=S) ->
+    D = D0*2,
+    case C + D of
+        N when N < Max ->
+            backoff(S#spec{cur=N, cur_dlta=D});
+        _ ->
+            backoff(S#spec{cur=Max})
+    end.
+
+backoff_normal(#spec{min=Min, dlta=Delta}=S) ->
+    backoff(S#spec{cur=Min, cur_dlta=Delta}).
