b17280e49bc7d735b4c7b2d2c9dec6ed049cb0cf
Essien Ita Essien <essiene@gmail.com>
2010-05-12 07:36:44 +0100

    Fix the SMS message formatter once for all
    And then log the silly bugger HTTP errors and SOAP errors
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/util.erl
+++ src/util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([soap_request/5, sms_response/2, smsc_params/0]).
+-export([soap_request/5, sms_response/2, smsc_params/0, sms_format_msg/2]).
 
 -include("simreg.hrl").
 
@@ -7,8 +7,8 @@
 -define(SMS_ERR_SRC, "SErr").
 -define(NOTIFY_MSISDN, "2347034494316").
 -define(MSG_SVC_UNAVAIL, "This service is temporarily unavailable. Please try again later").
+-define(SMS_MSG_MAX_LEN, 130).
 
--define(FORMAT_MSG(Fmt, Args), lists:flatten(io_lib:format(Fmt, Args))).
 
 soap_request(Url, RqHdrs, RqBody, RsFun, Op) ->
     try ibrowse:send_req(Url, RqHdrs, post, RqBody) of
@@ -16,18 +16,22 @@ soap_request(Url, RqHdrs, RqBody, RsFun, Op) ->
             S = RsFun(RsBody),
             S#soap_response{op=Op};
         {ok, Status, _, _} -> 
+            error_logger:error_msg("[~p] Got HTTP ~p while calling ~p~n", [self(), Status, Url]),
             Msg = "HTTP " ++ Status,
             #soap_response{status=Status, message=Msg, op=Op};
-        {error, Reason} ->
-            Msg = ?FORMAT_MSG("error: ~p", [Reason]),
-            #soap_response{status=1000, message=Msg}
+        {error, Reason}=Error ->
+            error_logger:error_msg("[~p] Got ~p while calling ~p~n", [self(), Error, Url]),
+            Msg = sms_format_msg("error: ~p", [Reason]),
+            #soap_response{status=1000, message=Msg, op=Op}
     catch 
         Type:Message ->
-            Msg = ?FORMAT_MSG("~p : ~p", [Type, Message]),
+            error_logger:error_msg("[~p] Got ~p while calling ~p~n", [self(), {Type, Message}, Url]),
+            Msg = sms_format_msg("~p : ~p", [Type, Message]),
             #soap_response{status=1000, message=Msg, op=Op}
     end.
 
 sms_response(Dest, #soap_response{message=undefined}) ->
+    error_logger:error_msg("[~p] Was unable to get a soap response~n", [self()]),
     sms:send(?SMS_SRC, Dest, ?MSG_SVC_UNAVAIL),
     sms:send(?SMS_ERR_SRC, ?NOTIFY_MSISDN, "Unable to get response");
 
@@ -37,9 +41,10 @@ sms_response(Dest, #soap_response{status=0, message=Msg}) ->
 sms_response(Dest, #soap_response{status=100, op=reg, message=Msg}) ->
     sms:send(?SMS_SRC, Dest, Msg);
 
-sms_response(Dest, #soap_response{status=N, message=Msg}) ->
+sms_response(Dest, #soap_response{status=N, message=Msg}=Res) ->
+    error_logger:error_msg("[~p] Got soap response: ~p~n", [self(), Res]),
     sms:send(?SMS_SRC, Dest, ?MSG_SVC_UNAVAIL),
-    Msg1 = ?FORMAT_MSG("~p~n~p", [N, Msg]),
+    Msg1 = sms_format_msg("~p~n~p", [N, Msg]),
     sms:send(?SMS_ERR_SRC, ?NOTIFY_MSISDN, Msg1);
 
 sms_response(Dest, Msg) ->
@@ -51,3 +56,17 @@ smsc_params() ->
     {ok, SystemId} = application:get_env(smsc_username), 
     {ok, Password} = application:get_env(smsc_password), 
     {Host, Port, SystemId, Password}.
+
+sms_format_msg(Fmt, Args) ->
+    S = lists:flatten(io_lib:format(Fmt, Args)),
+    case length(S) of
+        0 ->
+            "Empty Msg";
+        N when N < 130 -> 
+            S;
+        _ ->
+            S0 = string:substr(S, 1, ?SMS_MSG_MAX_LEN),
+            S0 ++ "..."
+    end.
+        
+        
