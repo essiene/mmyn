15a712da16d7277dcac928900081e5997f592ab0
Essien Ita Essien <essiene@gmail.com>
2010-09-17 01:09:23 +0100

    Modify txq.erl for transmitter queue logging
    - Ensure we can only push #txq_req{} records onto
      the queue
    
    - Start a new logger and file appender for logging here.
    
    - File will be txqueue.log
    
    - Track t1 when #txq_req{} is being enqued.
    
    - Implement tid/1 and log/4 to to generate transaction
      id from t1 value and to log to txqueue.log file
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmayen/txq.erl
+++ src/mmayen/txq.erl
@@ -1,17 +1,19 @@
 -module(txq).
 -behaviour(gen_server).
+-define(LOGGER, '__txq_logger').
+-include("simreg.hrl").
 -export([init/1,handle_call/3,
         handle_cast/2,handle_info/2,
         terminate/2,code_change/3]).
 
--export([start_link/0, push/1, pop/0, ping/0]).
+-export([start_link/0, push/1, pop/0, ping/0, log/4]).
 
 -record(st, {q}).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-push(Item) ->
+push(#txq_req{}=Item) ->
     gen_server:call(?MODULE, {push, Item}).
 
 pop() ->
@@ -22,10 +24,35 @@ ping() ->
 
 
 init([]) ->
-    {ok, #st{q=queue:new()}}.
+    {ok, LogDir} = application:get_env(qlog_logdir),
+    {ok, LogSize} = application:get_env(qlog_logsize),
+    {ok, NumRotations} = application:get_env(qlog_logkeep),
+    LogFile = "txqueue",
+    Suffix = "log",
 
-handle_call({push, Item}, _F, #st{q=Q}=St) ->
-    Q1 = queue:in(Item, Q),
+    AddAppender = fun () ->
+        case log4erl:add_file_appender(?LOGGER, file_logger_qlog, {LogDir, LogFile, {size, LogSize}, NumRotations, Suffix, all, "%l%n"}) of
+            {ok, _} ->
+    			{ok, #st{q=queue:new()}};
+            {error, {already_started, _}} ->
+    			{ok, #st{q=queue:new()}};
+            {error, Reason} ->
+                {stop, Reason}
+        end
+    end,
+
+    case log4erl:add_logger(?LOGGER) of
+        {ok, _} ->
+            AddAppender();
+        {error, {already_started, _}} ->
+            AddAppender();
+        {error, Reason} ->
+            {stop, Reason}
+    end.
+
+
+handle_call({push, #txq_req{}=Item}, _F, #st{q=Q}=St) ->
+    Q1 = queue:in(Item#txq_req{t1=now()}, Q),
     {reply, ok, St#st{q=Q1}};
 
 handle_call(pop, _F, #st{q=Q}=St) ->
@@ -54,3 +81,16 @@ terminate(_R, _St) ->
 
 code_change(_OldVsn, St, _Extra) ->
     {ok, St}.
+
+
+tid(T1) ->
+    {MegaSecs, Secs, MicroSecs} = T1,
+    lists:flatten(io_lib:format("~6.10.0B~6.10.0B~6.10.0B", [MegaSecs, Secs, MicroSecs])).
+
+
+log(#txq_req{t1=T1, src=Src, dst=Dst, message=Msg}, TxId, Tq, Tsend) ->
+	% Tid | Src | Dst | Msg | TxId | Pid | Tq | Tsend
+	Pid = self(),
+	Tid = tid(T1),
+	Log = lists:flatten(io_lib:format("~s|~s|~s|~s|~p|~p|~.2f|~.2f", [Tid, Src, Dst, Msg, TxId, Pid, Tq, Tsend])),
+	ok = log4erl:log(?LOGGER, debug, "~s", [Log]).
