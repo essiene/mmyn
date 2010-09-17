c702137702f7bc19423b13d41bc9d05c30e71d08
Essien Ita Essien <essiene@gmail.com>
2010-09-17 01:07:15 +0100

    Add file 't1' to #txq_req
    This tracks time T1, the time when the #txq_req{} is
    enqueued. All other calculations will be based on this.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- include/simreg.hrl
+++ include/simreg.hrl
@@ -4,7 +4,7 @@
 
 -record(soap_response, {status, message, op, flag, raw_req, raw_res}).
 
--record(txq_req, {src, dst, message}).
+-record(txq_req, {src, dst, message, t1}).
 
 -define(SMSC_HOST, "10.200.213.4").
 -define(SMSC_PORT, 10000).
