1a348c51ba69f68e84178830b3ba9b1456de4429
Essien Ita Essien <essiene@gmail.com>
2011-01-08 15:29:14 +0100

    Update the tlog record
    Include worker pid and worker id fields, while replacing
    the rxpid field
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- include/tlog.hrl
+++ include/tlog.hrl
@@ -1,3 +1,3 @@
 -record(req, {datetime,seqnum,src,dst,msg}).
 -record(res, {rt,src,dst,msg,status,op,code,detail,extra}).
--record(tlog, {tid, node, rxid, rxpid, smsc, port, system_id, handler, req, res}).
+-record(tlog, {tid, node, rxid, wid, wpid, smsc, port, system_id, handler, req, res}).
