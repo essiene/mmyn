602a86134f7976f3cce6b25e482121ada1f543e2
Essien Ita Essien <essiene@gmail.com>
2011-01-09 17:17:00 +0100

    Implement the json parsing code
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmayen/http_handler.erl
+++ src/mmayen/http_handler.erl
@@ -16,9 +16,6 @@ get(Url0, Tid, From, To, Keywords, Msg) ->
 
     
 
-    %{reply, {To, From, string:join(Msg, " ")}, {ok, {echo, 0}}}.
-
-
 fetch_url(Url) ->
     case ibrowse:send_req(Url, [], get) of
         {ok, "200", _, Response} ->
@@ -31,5 +28,40 @@ fetch_url(Url) ->
             {noreply, {error, {ibrowse, 500, Detail}}}
     end.
 
-process_response(_) ->
-    {noreply, ok}.
+%%
+% The external process is expected to return a simple JSON
+% String that will be converted back to the response structure
+%
+% response() =  {
+%               "success_flag" : boolean(),
+%               "operation" : string(),
+%               "code": integer(),
+%               "details": string(),
+%               "sendresponse_flag": boolean(),
+%               "sms_response" : {
+%                               "from":string(),
+%                               "to":string(),
+%                               "message":string()},
+%              }
+%
+process_response(Body) ->
+    process_response(?json_to_record(http_response, Body));
+process_response(#http_response{sendresponse_flag=false}=R) ->
+    noreply(R);
+process_response(#http_response{sms_response=SmsResponse}=R) ->
+    Sr = ?json_to_record(SmsResponse),
+    reply(R, Sr).
+
+noreply(#http_response{}=R) ->
+    {noreply, to_status(R)}.
+
+reply(#http_response{}=R, #sms_response{}=Sr) ->
+    {reply, to_response(Sr), to_status(R)}.
+
+to_status(#http_response{success_flag=true, operation=Op, code=C, details=D}) ->
+    {ok, {Op, C, D}};
+to_status(#http_response{success_flag=false, operation=Op, code=C, details=D}) ->
+    {error, {Op, C, D}}.
+
+to_response(#sms_response{from=F, to=T, message=M}) ->
+    {F, T, M}.
