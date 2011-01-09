238e0aecc6c15e8e8835bb0105ed619aee50b90a
Essien Ita Essien <essiene@gmail.com>
2011-01-09 09:34:38 +0100

    Add gen_server artifacts
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmayen/rtable.erl
+++ src/mmayen/rtable.erl
@@ -2,10 +2,32 @@
 -behaviour(gen_server).
 -include("simreg.hrl").
 
+-export([init/1,handle_call/3,handle_cast/2,handle_info/2,
+         terminate/2,code_change/3]).
+
 -export([route/2]).
 
 -record(st_rtable, {t}).
 -record(sms_req, {from, to, msg}).
+
+
+init([]) ->
+    {ok, #st_rtable{}}.
+
+handle_call(R,_F,St) ->
+    {reply, {error, R}, St}.
+
+handle_cast(_,St) ->
+    {noreply, St}.
+
+handle_info(_, St) ->
+    {noreply, St}.
+
+terminate(_,_) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {ok, St}.
 
 %% Routing Table Structure
 % rtable() = [rule()]
