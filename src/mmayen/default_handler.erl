2bf61c93c9891e22346f1f03ffdf8ed82697e1c2
Essien Ita Essien <essiene@gmail.com>
2011-01-22 11:47:20 +0100

    Implement more handlers in the default_handler mod
    - err_noreply/5 to simulate an error that returns nothing to
      the subscriber
    - err_reply/5 to simulate an error that returns a reply to the
      subscriber
    - vsn/5 to return version information to the subscriber
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmayen/default_handler.erl
+++ src/mmayen/default_handler.erl
@@ -1,5 +1,5 @@
 -module(default_handler).
--export([noreply/5, echo/5, hello/5]).
+-export([noreply/5, echo/5, hello/5, err_noreply/5, err_reply/5, vsn/5]).
 
 
 noreply(_, _, _, _, _) ->
@@ -10,3 +10,41 @@ echo(_Tid, From, To, _Keywords, Msg) ->
 
 hello(_Tid, From, To, _Keywords, _Msg) ->
     {reply, {To, From, "Hello mmyn world!"}, {ok, {hello, 0}}}.
+
+
+err_noreply(_, _, _, _, _) ->
+    {noreply, {error, {test, 500, "Test generated error"}}}.
+
+err_reply(_,_,_,_,_) ->
+    {reply, 
+        {"mmynerr", "Test generated error"},
+        {error, {test, 500, "Test generated error"}}
+    }.
+
+vsn(_,_,_,_,_) ->
+    Eng = "Mmayen",
+    Release = "1",
+    Os = os_type(),
+    Reply = io_lib:format("Eng: ~w\nRel: ~w\nOs: ~w", [Eng, Release, Os]),
+
+    {reply, 
+        {"mmyn", lists:flatten(Reply)}, 
+        {ok, {vsn, 0}}
+    }.
+
+
+os_type() ->
+    case os:type() of
+        {Family, Name} ->
+            os_version(io_lib:format("~s/~s", [Family, Name]));
+        Family ->
+            os_version(io_lib:format("~s", [Family]))
+    end.
+
+os_version(TypeStr) ->
+    case os:version() of
+        {Maj, Min, Rel} ->
+            lists:flatten([TypeStr, io_lib:format("/~w.~w.~w", [Maj, Min, Rel])]);
+        VersionStr ->
+            lists:flatten([TypeStr, io_lib:format("/~w", [VersionStr])])
+    end.
