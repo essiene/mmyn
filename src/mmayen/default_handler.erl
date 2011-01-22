02c47802a590e9081087ceb312f4c79c9ddca5f7
Essien Ita Essien <essiene@gmail.com>
2011-01-22 12:30:05 +0100

    Fix vsn/5 and err_reply/5
    - Need to specify src and dst in reply spec
    - use lists:concat/1 instead of io_lib:format and lists:flatten combo
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmayen/default_handler.erl
+++ src/mmayen/default_handler.erl
@@ -1,5 +1,6 @@
 -module(default_handler).
 -export([noreply/5, echo/5, hello/5, err_noreply/5, err_reply/5, vsn/5]).
+-export([os_type/0, os_version/1]).
 
 
 noreply(_, _, _, _, _) ->
@@ -15,20 +16,20 @@ hello(_Tid, From, To, _Keywords, _Msg) ->
 err_noreply(_, _, _, _, _) ->
     {noreply, {error, {test, 500, "Test generated error"}}}.
 
-err_reply(_,_,_,_,_) ->
+err_reply(_,Msisdn,Shortcode,_,_) ->
     {reply, 
-        {"mmynerr", "Test generated error"},
+        {Shortcode, Msisdn, "Test generated error"},
         {error, {test, 500, "Test generated error"}}
     }.
 
-vsn(_,_,_,_,_) ->
+vsn(_,Msisdn,_,_,_) ->
     Eng = "Mmayen",
     Release = "1",
     Os = os_type(),
-    Reply = io_lib:format("Eng: ~w\nRel: ~w\nOs: ~w", [Eng, Release, Os]),
+    Reply = lists:concat(["Eng:", Eng, "\nRel:", Release, "\nOS:", Os]),
 
     {reply, 
-        {"mmyn", lists:flatten(Reply)}, 
+        {"mmyn", Msisdn, Reply}, 
         {ok, {vsn, 0}}
     }.
 
@@ -36,15 +37,15 @@ vsn(_,_,_,_,_) ->
 os_type() ->
     case os:type() of
         {Family, Name} ->
-            os_version(io_lib:format("~s/~s", [Family, Name]));
+            os_version(lists:flatten(io_lib:format("~w/~w", [Family, Name])));
         Family ->
-            os_version(io_lib:format("~s", [Family]))
+            os_version(lists:flatten(io_lib:format("~w", [Family])))
     end.
 
 os_version(TypeStr) ->
     case os:version() of
         {Maj, Min, Rel} ->
-            lists:flatten([TypeStr, io_lib:format("/~w.~w.~w", [Maj, Min, Rel])]);
+            lists:concat([TypeStr, "/", Maj, ".", Min, ".", Rel]);
         VersionStr ->
-            lists:flatten([TypeStr, io_lib:format("/~w", [VersionStr])])
+            lists:concat([TypeStr, "/", VersionStr])
     end.
