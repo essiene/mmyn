6cd568b53631a99bb6e95b74023b26f56496578e
Essien Ita Essien <essiene@gmail.com>
2011-10-05 04:57:22 +0100

    Add embedded yaws gen_server
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/mmayen/mmyn_yaws.erl
@@ -0,0 +1,78 @@
+-module(mmyn_yaws).
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1,handle_call/3,handle_cast/2,handle_info/2,terminate/2,
+         code_change/3]).
+
+-record(rcfg, {yaws_gconf, yaws_sconf}).
+-define(APPMODS, {appmods, [
+            {"v", msisdn_view}
+        ]}).
+-define(YAWS_NAME, "Mmayen/1.0.0").
+
+
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+
+init(_) ->
+    case load_config() of
+        {error, Reason} ->
+            {stop, Reason};
+        {ok, Config} ->
+            case apply_config(Config) of
+                {error, Reason} ->
+                    {stop, Reason};
+                ok -> 
+                    {ok, nil}
+            end
+    end.
+
+handle_call(R, _F, St) ->
+    {reply, {error, R}, St}.
+
+handle_cast(_, St) ->
+    {noreply, St}.
+
+handle_info(_, St) ->
+    {noreply, St}.
+
+terminate(_,_) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {ok, St}.
+
+
+load_config() ->
+    case application:get_env(mmyn, http_env) of
+        undefined ->
+            {error, {config_not_found, http_env}};
+        {ok, HttpEnv0} ->
+            HttpEnv = [{yaws, ?YAWS_NAME}|HttpEnv0],
+            Id = proplists:get_value(id, HttpEnv, "mmayen"),
+            case application:get_env(mmyn, http_server) of
+                undefined ->
+                    {error, {config_not_found, http_server}};
+                {ok, SrvEnv0} ->
+                    SrvEnv = [?APPMODS|SrvEnv0],
+                    DocRoot = proplists:get_value(docroot, SrvEnv, "/tmp"),
+                    {ok, SconfList, Gconf, _} = yaws_api:embedded_start_conf(DocRoot, SrvEnv, HttpEnv, Id),
+                    {ok, #rcfg{yaws_gconf=Gconf,yaws_sconf=SconfList}}
+            end
+    end.
+
+apply_config(#rcfg{}=R) ->
+    yaws_api:setconf(R#rcfg.yaws_gconf, R#rcfg.yaws_sconf).
+
+get_env(App, Key, Default) ->
+    case application:get_env(App, Key) of
+        undefined ->
+            {ok, Default};
+        N ->
+            N
+    end.
